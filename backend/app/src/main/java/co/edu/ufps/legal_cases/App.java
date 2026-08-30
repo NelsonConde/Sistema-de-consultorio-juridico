befd49451515288778d94e29bbdcc90ff66867e8
NelsonConde <nelsonenrriquect@ufps.edu.co>
2026-08-30 18:43:05 -0500

    test: make backend integration tests reproducible

--- backend/app/src/main/java/co/edu/ufps/legal_cases/App.java
+++ backend/app/src/main/java/co/edu/ufps/legal_cases/App.java
@@ -2,9 +2,7 @@ package co.edu.ufps.legal_cases;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class App {
 
